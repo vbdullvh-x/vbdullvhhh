b671240ccd96342e081392d95b678a20876da2fd
vbdullvh-x <abdullahehab72@gmail.com>
2024-09-03 09:27:54 +0300

    Add files via upload

---
+++ task vbdullvh/Task vbdullvh.js
@@ -0,0 +1,39 @@
+// let x = 10; 
+
+// function boda (){
+//     let x = 20 ; 
+//     console.log(x)
+// }
+// vbdullvh() // output => 20 local Scope 
+
+ /////////////////////////////////////
+
+
+// let y = 10; 
+
+// function vbdullvh (){
+//     console.log(y)
+// }
+// vbdullvh() // 10 Global Scope
+
+// /////////////////
+
+// var z = 17; 
+
+// function vbdullvh (){
+//     var z = 50 ;
+//     console.log(z)
+// }
+// vbdullvh() // 50 Local Group
+
+/////////////////////
+
+// let a = 15; 
+
+// function vbdullvh (){
+    
+//     console.log(a)
+// }
+// vbdullvh() // 15 Global Group
+
+
